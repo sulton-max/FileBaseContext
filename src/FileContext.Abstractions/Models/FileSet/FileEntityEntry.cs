5d5793e3775dfbd43bb4866522113bda5a0493e6
Max <sultonbek.rakhimov@gmail.com>
2023-08-31 01:24:08 +0500

    Create file entity entry

---
+++ src/FileContext.Abstractions/Models/FileSet/FileEntityEntry.cs
@@ -0,0 +1,20 @@
+﻿namespace FileContext.Abstractions.Models.FileSet;
+
+/// <summary>
+/// Provides information about entity state in a file base
+/// </summary>
+/// <typeparam name="TEntity">Entity type</typeparam>
+public class FileEntityEntry<TEntity>
+{
+    /// <summary>
+    /// The entity being tracked by a context
+    /// </summary>
+    public TEntity Data { get; set; }
+
+    /// <summary>
+    ///  The state in which an entity is being tracked by a context
+    /// </summary>
+    public FileEntityState State { get; set; }
+
+    public FileEntityEntry(TEntity data) => Data = data;
+}

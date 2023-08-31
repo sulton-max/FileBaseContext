60a0962bf1999342aab90b72ef3be3774101ffbd
Max <sultonbek.rakhimov@gmail.com>
2023-08-31 20:05:01 +0500

    Create FileSet web API demo

---
+++ src/FileContext.Samples.FileSet.Api/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using FileContext.Abstractions.Models.Entity;
+
+namespace FileContext.Samples.FileSet.Console.Models;
+
+public class BlogPost : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid AuthorId { get; set; }
+
+    public BlogPost(Guid id, string title, string content, DateTime createdAt, Guid authorId)
+    {
+        Id = id;
+        Title = title;
+        Content = content;
+        CreatedAt = createdAt;
+        AuthorId = authorId;
+    }
+}

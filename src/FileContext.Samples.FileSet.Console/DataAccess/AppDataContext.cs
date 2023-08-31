32f9c5371babb0f440912e64ab6d40e281b771e6
Max <sultonbek.rakhimov@gmail.com>
2023-08-31 17:37:16 +0500

    Create console sample for FileSet

---
+++ src/FileContext.Samples.FileSet.Console/DataAccess/AppDataContext.cs
@@ -0,0 +1,22 @@
+﻿using FileContext.Abstractions.Models.FileSet;
+using FileContext.Core.Models.FileSet;
+using FileContext.Samples.FileSet.Console.Models;
+
+namespace FileContext.Samples.FileSet.Console.DataAccess;
+
+public class AppDataContext
+{
+    public IFileSet<User, Guid> Users = new FileSet<User, Guid>(Directory.GetCurrentDirectory(), null, null);
+    public IFileSet<BlogPost, Guid> Posts = new FileSet<BlogPost, Guid>(Directory.GetCurrentDirectory(), null, null);
+
+    public AppDataContext()
+    {
+        Task.WaitAll(Users.FetchAsync().AsTask(), Posts.FetchAsync().AsTask());
+    }
+
+    public async ValueTask SaveChangesAsync()
+    {
+        await Users.SaveChangesAsync();
+        await Posts.SaveChangesAsync();
+    }
+}

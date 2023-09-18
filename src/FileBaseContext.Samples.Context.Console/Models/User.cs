c9bf5e2d978cbe8930a15dcd23233372ffb4acf3
Max <sultonbek.rakhimov@gmail.com>
2023-09-18 12:04:58 +0500

    Create console demo for FileContext

---
+++ src/FileBaseContext.Samples.Context.Console/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace FileBaseContext.Samples.Context.Console.Models;
+
+public class User : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public User(Guid id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
